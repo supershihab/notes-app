e03e0006b64f4ea9797fc0f3cc48a70f9324be9a
Shihab Mahmud <shihabmahmud471@gmail.com>
2023-09-12 19:39:15 +0600

    Add first strutcuted route with controller and app

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,2 +1,14 @@
 'use strict';
 
+/*
+* GET HOMEPAGE
+*/
+exports.homepage = async (req, res) => {
+  const locals = {
+    title: "Notes App",
+    description: "Free Note Taking App by Shihab Mahmud",
+  };
+  res.render('index', locals);
+
+}
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,19 @@
 'use strict';
 
+//we need bring express since we need to create router  here
+const express = require('express');
+const router = express.Router();
+
+//bring the controller for business logic
+const mainController = require('../controllers/mainController');
+
+/*
+* App Routes
+*/
+
 //create a basic route to render views/index.ejs
-app.get('/', (req, res) => {
-  const locals = {
-    title: "Notes App",
-    description: "Free Note Taking App by Shihab Mahmud",
-  };
-  res.render('index', locals);
-});
+router.get('/', mainController.homepage); //homepage can be named anything
+
+
+//export the router for app.js
+module.exports = router;
