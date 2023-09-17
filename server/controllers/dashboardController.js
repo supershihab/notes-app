e456ea56f7e7b9aab736c63c6c4b7ce27d7931f5
DESKTOP-G6PTLPB\ShihabMahmud <shihabmahmud471@gmail.com>
2023-09-17 12:16:14 +0600

    Add notes and show notes

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,15 +1,30 @@
 'use strict';
+const Note = require('../models/Notes');
+const mongoose = require('mongoose');
+
 
 /*
 * GET DASHBOARD
 */
 exports.dashboard = async (req, res) => {
+
   const locals = {
     title: "Dashboard",
     description: "Free Note Taking App by Shihab Mahmud",
   };
-  res.render('dashboard/index', {
-    locals,
-    layout: '../views/layouts/dashboard',
-  });
+
+  try {
+    const notes = await Note.find({});
+    // console.log(notes);
+    res.render('dashboard/index', {
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: '../views/layouts/dashboard',
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
 }
